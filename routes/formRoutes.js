dff580bb57b03f0ba31d1ad62d8eb8192fe0d5cc
ayanjamil <ayanjamil00@gmail.com>
2024-10-02 20:29:09 +0530

    feat: basic excell export working

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -6,7 +6,7 @@ const Form = require("../models/Form");
 // const auth = require("../middleware/auth");
 const auth = require("../ middleware/auth");
 const sendEmail = require("../config/nodemailer");
-const User = require("../models/User");
+const excel = require("exceljs");
 
 // Protect the form creation route
 router.post("/generate-link", auth, async (req, res) => {
@@ -45,6 +45,56 @@ router.get("/all-forms", auth, async (req, res) => {
   }
 });
 
+router.get("/export/:formId", async (req, res) => {
+  try {
+    const { formId } = req.params;
+    const form = await Form.findOne({ formId });
+
+    if (!form || !form.submissions || form.submissions.length === 0) {
+      return res.status(404).send("No submissions found for this form.");
+    }
+
+    // Create a new Excel workbook
+    const workbook = new excel.Workbook();
+    const worksheet = workbook.addWorksheet("Submissions");
+
+    // Add column headers (Adjust this based on your form fields)
+    worksheet.columns = [
+      { header: "S.No", key: "s_no", width: 10 },
+      { header: "Name", key: "name", width: 30 },
+      { header: "Email", key: "email", width: 30 },
+      // Add other form fields if necessary
+    ];
+
+    // Map form submissions to worksheet rows
+    console.log("SUBMISSION : ", form.submissions);
+    form.submissions.forEach((submission, index) => {
+      // Since submission is a Map, use get() to retrieve the values
+      worksheet.addRow({
+        s_no: index + 1,
+        name: submission.get("name"),
+        email: submission.get("email"),
+      });
+    });
+
+    // Send the Excel file
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=" + "form-submissions.xlsx"
+    );
+
+    await workbook.xlsx.write(res);
+    res.end();
+  } catch (error) {
+    console.error("Error exporting data:", error);
+    res.status(500).send("Error exporting form submissions");
+  }
+});
+
 // Serve the form based on the generated link
 router.get("/form/:id", async (req, res) => {
   const { id } = req.params;
@@ -70,6 +120,8 @@ router.post("/submit-form", async (req, res) => {
     const form = await Form.findOne({ formId });
     console.log("FORM : ", form);
     if (!form) return res.status(404).send("Form not found");
+    form.submissions.push(formData);
+    await form.save(); // Save the form with the new submission
 
     // Fetch the user who created the form
     // const userEmail = await User.findOne({ email: form.createdBy });
